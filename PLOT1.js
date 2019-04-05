1d58206cecd8ad28c4d020179ef25bcd97bf1622
alpharou <alpharou.dev@gmail.com>
2019-04-05 16:51:11 +0200

    Added Scale and Offset to the Canvas_view

--- PLOT1.js
+++ PLOT1.js
@@ -2,26 +2,70 @@ function setup() {
 	
 	createCanvas(windowWidth, windowHeight);
 	
-	scale = 1;
+	//flag Variables
+	stretchScale = false;
+	doOffset = true;
+	
+	//Global variables
+	wO = 0; //widthOffset
+	hO = 0; //heightOffset
+	wS = 1; //widthScale
+	hS = 1; //heightScale
+	
+	doScale();
 	plotter1 = new Plotter();
   
 }
 
 function draw() {
 	
-	scale(getScale());
+	//Scale
+	if (stretchScale) {
+		scale(wS, hS);
+	} else {
+		scale(min(wS,hS));
+	}
+	
 	background(255);
+	noFill();
+	strokeWeight(2);
+	rect(0 + wO, 0 + hO, 1920, 1080);
+	line(0 + wO, 0 + hO, 1920 + wO, 1080 + hO);
+	rect(40 + wO ,40 + hO, 1840, 1000);
 	
 }
 
-function getScale() {
+function doScale() {
 
-	return 1;
+	//True workspace size is 1920x1080px
+	wS = windowWidth/1920;
+	console.log("wS = " + wS);
+	hS = windowHeight/1080;
+	console.log("hS = " + hS);
 	
+	//Calculate offsets to center the piece on the canvas
+	if (!stretchScale && doOffset) {
+		wO = (windowWidth - 1920*min(wS,hS)) / min(wS,hS); //WTF OFFSET??
+		wO /= 2;
+		console.log("wO = " + wO);
+		hO = (windowHeight - 1080*min(wS,hS)) / min(wS,hS); //EXCUSE WTF??
+		hO /= 2;
+		console.log("hO = " + hO);
+	}
+	
+	return [wS, hS];
+
 }
 
 function mouseMoved() {
 	
 	
+	
+}
+
+function windowResized() {
+	
+	resizeCanvas(windowWidth, windowHeight);
+	doScale();
 	
 }
